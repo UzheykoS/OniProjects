703e6c6cdb89d03411f4cc4c7040c81258dbe61f
Sergey Uzheyko <sergio.uzheyko@gmail.com>
2017-09-01 23:04:57 +0300

    Added basic media queries.

--- OniWebSite/src/components/MacaronSingle.tsx
+++ OniWebSite/src/components/MacaronSingle.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { Nav } from "./Nav";
 import { Tabs } from "../Helper"
+import * as Media from 'react-media'
 
 interface IMacaronSingleProps {
     name: string;
@@ -41,27 +42,53 @@ export class MacaronSingle extends React.Component<IMacaronSingleProps, any>{
         }
         else {
             return <div>
-                <div className="row">
-                    <div className="col-md-6" >
-                        <div className="macaron-desc-container">
-                            <div className="taste-name">
-                                {name}
+                <Media query={{ maxWidth: 800 }}>
+                    {matches => matches ? (
+                        <div className="row">
+                            <div className="col-md-5">
+                                <div className="macaron-image-container">
+                                    <img src={imageUrl} />
+                                </div>
                             </div>
-                            <div className="taste-desc">
-                                {shortDescription}
+                            <div className="col-md-6">
+                                <div className="macaron-desc-container">
+                                    <div className="taste-name">
+                                        {name}
+                                    </div>
+                                    <div className="taste-desc">
+                                        {shortDescription}
+                                    </div>
+                                    <div className="taste-body">
+                                        {fullDescription}
+                                    </div>
+                                </div>
                             </div>
-                            <div className="taste-body">
-                                {fullDescription}
-                            </div>
+                            <div className="col-md-1" />
                         </div>
-                    </div>
-                    <div className="col-md-5">
-                        <div className="macaron-image-container">
-                            <img src={imageUrl} />
-                        </div>
-                    </div>
-                    <div className="col-md-1" />
-                </div>
+                    ) : (
+                            <div className="row">
+                                <div className="col-md-6" >
+                                    <div className="macaron-desc-container">
+                                        <div className="taste-name">
+                                            {name}
+                                        </div>
+                                        <div className="taste-desc">
+                                            {shortDescription}
+                                        </div>
+                                        <div className="taste-body">
+                                            {fullDescription}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-5">
+                                    <div className="macaron-image-container">
+                                        <img src={imageUrl} />
+                                    </div>
+                                </div>
+                                <div className="col-md-1" />
+                            </div>
+                        )}
+                </Media>
             </div>;
         }
     }
