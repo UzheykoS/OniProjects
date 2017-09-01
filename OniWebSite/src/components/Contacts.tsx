33260ea0bce69bd8d0363635fbc2c8e8dc429730
Serhii Uzheiko <SUzheyko@vertice.local>
2017-09-01 18:56:05 +0300

    Moved to px font sizes; added raw google map.

--- OniWebSite/src/components/Contacts.tsx
+++ OniWebSite/src/components/Contacts.tsx
@@ -1,14 +1,57 @@
 import * as React from 'react'
 import { Nav } from "./Nav";
 import { Tabs } from "../Helper"
+import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+
+const googleMapURL = "https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=AIzaSyDH_aHRsVOr_CMITd6m0Vuo1X2qSXMicdY"
+
+const GettingStartedGoogleMap = withGoogleMap(props => (
+    <GoogleMap
+        ref={props.onMapLoad}
+        defaultZoom={3}
+        defaultCenter={{ lat: 50.4544477, lng: 30.6165073 }}
+        onClick={props.onMapClick}
+    >
+    </GoogleMap>
+));
 
 export class Contacts extends React.Component<any, any>{
     render() {
-        return (
-            <div>
-                <Nav tab={Tabs.Contacts}/>
-                Contacts
+        return <div className="contacts-container">
+            <Nav tab={Tabs.Contacts} />
+            <div className="contacts-header">
+                <div className="row">
+                    <div className="col-md-5">
+                        <div className="contacts-photo">
+                            <img src="./images/mac_large.png" />
+                        </div>
+                    </div>
+                    <div className="col-md-7">
+                        <div className="header-main">О нас</div>
+                        <div className="header-desc">Очень вкусные дессерты</div>
+                        <div className="header-body">А тут много текста
+                                Lorem ipsum dolor sit amet, congue gravida erat et nullam, quis habitant varius convallis pellentesque eros. Fringilla montes ut enim id pellentesque consectetuer, erat magna sed etiam a viverra ut, fringilla nec sit orci curabitur ut risus, tristique platea ultricies aliquam venenatis curabitur.
+
+In scelerisque nisl sit neque lorem, id integer ut commodo nunc purus neque, vel in leo, pede eos ipsum, dui commodo metus. </div>
+                    </div>
+                </div>
             </div>
-        );
+            <div className="contacts-body">
+                <GettingStartedGoogleMap
+                    containerElement={
+                        <div style={{ height: `100%` }} />
+                    }
+                    mapElement={
+                        <div style={{ height: `100%` }} />
+                    }
+                    onMapLoad={() => { }}
+                    onMapClick={() => { }}
+                    onMarkerRightClick={() => { }}
+                />
+            </div>
+            <div className="contacts-footer">
+                <img src="/images/Oni_logo.png" />
+            </div>
+        </div>;
     }
 };
