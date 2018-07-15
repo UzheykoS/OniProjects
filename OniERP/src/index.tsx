be453f787812a09bc9d8b0b7f9a3a1de520b05df
Sergey Uzheyko <sergio.uzheyko@gmail.com>
2018-07-15 16:06:03 +0300

    Creating project

---
+++ OniERP/src/index.tsx
@@ -0,0 +1,32 @@
+import * as React from "react"
+import TestComponent from './components/TestComponent'
+import { configureOauth } from "./utils/oauth2";
+import Helper, { OAuth2Info } from "./utils/helper";
+import './styles/global.scss';
+import axios from 'axios';
+
+export interface AppProps {
+}
+
+export interface AppState {
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        var instance = axios.create({
+            baseURL: "",//WebApiUrl
+            headers: { 'X-Custom-Header': 'foobar' }
+        });
+    }
+
+    render() {
+        return <div>
+            <TestComponent label="Hello World!" />            
+        </div>;
+    }
+}
+
---
+++ OniERP/webpack.config.ts
@@ -0,0 +1,50 @@
+﻿
+const environment = process.env.NODE_ENV.trim();
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+import { Configuration } from 'webpack';
+import commonConfig from "./webpack.common";
+import * as path from 'path';
+
+if (environment === "production") {
+    const externals: any = {
+        'react': {
+            root: 'React',
+            commonjs2: 'react',
+            commonjs: 'react',
+            amd: 'react',
+            umd: 'react'
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+            umd: 'react-dom'
+        },
+        'axios': {
+            root: 'axios',
+            commonjs2: 'axios',
+            commonjs: 'axios',
+            amd: 'axios',
+            umd: 'axios'
+        }
+    };
+    commonConfig.externals = externals;
+    commonConfig.devtool = 'source-map';
+} 
+else {
+    commonConfig.entry = "./test/index.tsx";
+    commonConfig.output = {
+        filename: "bundle.js",
+        path: path.join(__dirname, 'dist')
+    };
+    commonConfig.devtool = 'inline-source-map';  
+    commonConfig.plugins.push(
+        new HtmlWebpackPlugin({
+            title: "Template Test"
+        })
+    )
+}
+
+export default commonConfig;
