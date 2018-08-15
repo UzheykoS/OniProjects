5d734d3f80091dd4cdddaefe7be92111a96109db
Sergey Uzheyko <sergio.uzheyko@gmail.com>
2018-08-15 23:53:43 +0300

    Added build step.

--- OniERP/server.js
+++ OniERP/server.js
@@ -10,3 +10,24 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, function () {    
   console.log('Listening at http://localhost:3000');  
 });
+
+
+
+// Webpack Dev Server for Hot module reloading
+// Comment it out during production
+// const webpack = require(`webpack`);
+// const WebpackDevServer = require(`webpack-dev-server`);
+// const config = require(`./webpack.dev`);
+
+// new WebpackDevServer(webpack(config), {
+//   publicPath: config.output.publicPath,
+//   hot: true,
+//   historyApiFallback: true,
+//   stats: {colors: true}
+// }).listen(3001, `localhost`, function (err, result) {
+//   if (err) {
+//     return console.log(err);
+//   }
+
+//   console.log(`Listening at http://localhost:3001/`);
+// });
