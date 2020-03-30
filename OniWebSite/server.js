62f45ecfd3af8a4d3903fec75a1915670303e4af
Serhii Uzheiko <suzheiko@emergetms.com>
2020-03-30 16:54:25 +0300

    Improved server.js

--- OniWebSite/server.js
+++ OniWebSite/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
-const result = dotenv.config()
+const result = dotenv.config();
 
 if (result.error) {
-  throw result.error
+  throw result.error;
 }
 
 app.use(express.static(path.join(__dirname, 'dist')));
@@ -14,6 +14,45 @@ const routes = require('./routes.js');
 app.use('/', routes);
 
 const PORT = result.parsed.PORT || 8080;
-app.listen(PORT, function() {
+const server = app.listen(PORT, function() {
   console.log(`Server Running on port ${PORT}`);
 });
+
+setInterval(
+  () =>
+    server.getConnections((err, connections) =>
+      console.log(`${connections} connections currently open`)
+    ),
+  1000
+);
+
+process.on('SIGTERM', shutDown);
+process.on('SIGINT', shutDown);
+
+let connections = [];
+
+server.on('connection', connection => {
+  connections.push(connection);
+  connection.on(
+    'close',
+    () => (connections = connections.filter(curr => curr !== connection))
+  );
+});
+
+function shutDown() {
+  console.log('Received kill signal, shutting down gracefully');
+  server.close(() => {
+    console.log('Closed out remaining connections');
+    process.exit(0);
+  });
+
+  setTimeout(() => {
+    console.error(
+      'Could not close connections in time, forcefully shutting down'
+    );
+    process.exit(1);
+  }, 10000);
+
+  connections.forEach(curr => curr.end());
+  setTimeout(() => connections.forEach(curr => curr.destroy()), 5000);
+}
